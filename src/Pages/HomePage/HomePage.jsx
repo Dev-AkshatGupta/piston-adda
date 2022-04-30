a76d1fed7fa231644ef9f64c9d671f909a032b9c
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-04-30 15:26:31 +0530

    posts created for the database

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,7 +5,11 @@ import { RightAside } from "Components/RightAside/RightAside";
 import { Post } from "Components/Post/Post";
 import { PostInput } from "Components/PostInput/PostInput";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 function HomePage() {
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const loadingStatus = useSelector((state) => state.auth.loading);
+  console.log(currentUser);
   return (
     <div className="layout">
       <div className="layout__left-sidebar">
